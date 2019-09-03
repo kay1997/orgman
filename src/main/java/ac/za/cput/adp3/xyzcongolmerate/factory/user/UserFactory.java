058f897cfd45af6c2ab87a56ea391b53da7ca813
Kaylen Abrahams <kaylenabrahams10@gmail.com>
2019-09-03 20:37:28 +0200

    final

--- src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserFactory.java
+++ src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserFactory.java
@@ -4,7 +4,6 @@ import ac.za.cput.adp3.xyzcongolmerate.domain.user.User;
 
 public class UserFactory {
 
-    //TODO: implement method body ONLY!
     public static User buildUser(String userEmail, String firstName, String lastName) {
         return new User.Builder().userEmail(userEmail)
                 .firstName(firstName)
